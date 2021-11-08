cc56b9237fb7cab3f1fc15c5d1b45181931b4fdd
shawnnettleton <shawnnettleton77@gmail.com>
2021-11-08 14:11:59 -0800

    Making minor changes to previous code structure. Also added in reverse_preorder to traversal.py and test.py.

--- src/traversal.py
+++ src/traversal.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 
 """
-Last edited by   : Braiden
-Last edited time : 07/11/2021
+Last edited by   : Shawn
+Last edited time : 08/11/2021
 Version Status: stable
 TO DO: Verify correctness
 """
+
+
+def reverse_preorder(root):
+    """
+    @ input:   root of lcrs tree
+    @ output:  integer list of id's reverse preorder
+    """
+    node_list = []
+    temp_stack = [root]
+    while len(temp_stack) != 0:
+        curr = temp_stack.pop()
+        node_list.append(curr.value)
+        if curr.child is not None:
+            temp_stack.append(curr.child)
+        if curr.next is not None:
+            temp_stack.append(curr.next)
+    return node_list
+
 
 def in_order_traversal(root):
     """
@@ -14,7 +32,7 @@ def in_order_traversal(root):
     """
     node_list = []
     if root:
-        node_list = in_order_traversal(root.child)
-        node_list.append(root.value)
-        node_list = node_list + in_order_traversal(root.next)
+        node_list = in_order_traversal(root.child)  # Left tree
+        node_list.append(root.value)  # Root of tree
+        node_list = node_list + in_order_traversal(root.next)  # Right tree
     return node_list
