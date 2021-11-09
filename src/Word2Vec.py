1a0ffcc894b4b87c1a1e45e837ee48bae0f301df
Roy <liqiaowangg@gmail.com>
2021-11-08 17:47:11 -0800

    seq-splitting implementation

--- src/Word2Vec.py
+++ src/Word2Vec.py
@@ -1,53 +1,46 @@
 #!/usr/bin/python3
 
 """
-Last edited by   : Shawn
+Last edited by   : Roy
 Last edited time : 08/11/2021
-Version Status: stable
-TO DO: None
+Version Status: dev
+TO DO: Verify correctness
 """
 
 
 class new_Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, json_object):
+        self.id    = json_object['id']
+        self.type = json_object['type']
         self.child = None
         self.next = None
+        self.parent=None
+        try:
+            self.value   = json_object['value']
+        except KeyError as err:
+            self.value   = None
 
-
-"""
-this object could be used in the further step, for this step, we use the unique id to present it.
-"""
-
-
-# class Node_ast:
-#     def __init__(self , json_object):
-#         self.id    = json_object['id']
-#         self.type  = json_object['type']
-#         try:
-#             self.value   = json_object['value']
-#         except KeyError as err:
-#             self.value   = None
-
-
-def addSibling(node, value):
+def addSibling(parent_node,node,child_node):
     if node is None:
         return None
 
     while node.next is not None:
         node = node.next
-    node.next = new_Node(value)
+
+    node.next = child_node
+    node.next.parent=parent_node
     return node.next
 
 
-def addChild(node, value):
+def addChild(node, child):
     if node is None:
         return None
 
     if node.child is not None:
-        return addSibling(node.child, value)
+        return addSibling(node,node.child,child)
     else:
-        node.child = new_Node(value)
+        node.child = child
+        node.child.parent=node
         return node.child
 
 
@@ -55,7 +48,7 @@ def find_node(candidateNode, targetNode):
     if candidateNode is None:
         return None
 
-    if candidateNode.value == targetNode:
+    if candidateNode.id == targetNode:
         return candidateNode
     # find left side
     left_node = find_node(candidateNode.child, targetNode)
@@ -68,22 +61,54 @@ def find_node(candidateNode, targetNode):
 def ast2lcrs(ast):
     """
     @ input : AST tree
-    @ output: LCRS tree
+    @ output: LCRS tree, lookup_table
     """
     root = None
-    token_list = {}  # key: ast_id , value : coressponding Node_ast
+    lookup_table = {}  # key: ast_id , value : coressponding Node_object
     for token_id in range(len(ast)):  # loop through a single AST
         if root is None:
-            root = new_Node(ast[token_id]['id'])
+            root = new_Node(ast[token_id])
+            lookup_table[token_id]=root
             for child in ast[token_id]['children']:
-                token_list[child] = addChild(root, child)
+                lookup_table[child] = addChild(root, new_Node(ast[child]))
         else:
             # find out the location of node
             starting_node = find_node(root, token_id)  # raise error if we cannot find the node. After we find it, we would append the child under this node
             if starting_node is not None:
                 try:
                     for child in ast[token_id]['children']:
-                        token_list[child] = addChild(starting_node, child)
+                        lookup_table[child] = addChild(starting_node, new_Node(ast[child]))
                 except KeyError as err:
                     pass
-    return root
+    return root,lookup_table
+
+def sequenceSplit(in_order_list,lookup_table):
+    """
+    @input : an entire ast list
+    @output: sub_sequence table -> Key: training_id, Value: sub_sequence
+    """
+    subSequence_list={} 
+    trainning_dataset_index=0
+    list=[]
+    for index in in_order_list:
+        if(lookup_table[index].value is None):
+            list.append(lookup_table[index].type)
+        else:
+            list.append(lookup_table[index].value)
+
+        if(lookup_table[index].next != None):
+            # we need to get the parent nodes starting from the end of "in_order_list"
+            number_of_parent=1
+            seeking_node=lookup_table[index] # we use this seeking node to find out the number of parents we have.
+            while(seeking_node.parent.id != 0):
+                number_of_parent+=1
+                seeking_node=seeking_node.parent
+            # now we extract the parents nodes 
+            while(-number_of_parent<0):
+                list.append(lookup_table[in_order_list[-number_of_parent]].type)
+                number_of_parent-=1
+            
+            subSequence_list[trainning_dataset_index]=list # terminate the list 
+            trainning_dataset_index+=1 # add up the index number
+            list=[] # earse the whole list
+    return subSequence_list
