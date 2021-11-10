829c872c9ac7388bec6d2f920a00c19971cb3cec
Roy <liqiaowangg@gmail.com>
2021-11-09 22:32:12 -0800

    dev updated

--- src/test.py
+++ src/test.py
@@ -13,13 +13,10 @@ TO DO: Verify correctness
 
 if __name__ == "__main__":
     ast_data = sc.storage_connection(str(sys.argv[1]))  # call storage_connection() to get the ast
-    seq_table={} # key: id, value: sub_seq
-    seq_table_index=0
-    for ast_id in range(len(ast_data)):
-        root,lookup_table = w2v.ast2lcrs(ast_data[ast_id])  # call ast2lcrs() to convert the ast to lcrs
-        in_order_list = tr.in_order_traversal(root) # call in_order_traversal() to get the sequence.
-        subSequence_list=w2v.sequenceSplit(in_order_list,lookup_table)
-        for list in range(len(subSequence_list)):
-            seq_table[seq_table_index]=subSequence_list[list]
-            seq_table_index+=1
-    print(seq_table)
+    root, lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
+    in_order_list = tr.in_order_traversal(root)  # call in_order_traversal() to get the sequence.
+    subSequence_list = w2v.sequenceSplit(in_order_list, lookup_table)
+
+    tokenized_subSequence, tokenized_lookup_table = w2v.tokenization(subSequence_list)
+    print(tokenized_lookup_table[1])
+    # print(tokenized_subSequence)
---
+++ src/word_embedding.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import tensorflow as tf
+from tensorflow.keras import layers
+"""
+Last edited by   : Roy
+Last edited time : 09/11/2021
+Version Status: dev
+TO DO: Skipgram implementaion with tf
+"""
+
+
+def storage_connection(tokenized_subsequence, lookup_table_dict):
+    """
+    @ input : tokenized_dict --> input dict just replace words with int representation
+                and lookup_table_dict --> {1, [R = 'Program, F = 12],...}
+    @ output: word embedding table
+    """
+    pass
+
