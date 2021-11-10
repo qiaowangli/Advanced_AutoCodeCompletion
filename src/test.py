c72b7ea48084bdfb7b69645c8b7aad537e3efdbf
shawnnettleton <shawnnettleton77@gmail.com>
2021-11-09 18:39:30 -0800

    Adding in the tokenization functionality

--- src/test.py
+++ src/test.py
@@ -12,10 +12,13 @@ TO DO: Verify correctness
 """
 
 if __name__ == "__main__":
-    ast_data = sc.storage_connection(str(sys.argv[1]))  # call storage_connection() to get the ast
-    root,lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
-    in_order_list = tr.in_order_traversal(root) # call in_order_traversal() to get the sequence.
-    subSequence_list=w2v.sequenceSplit(in_order_list,lookup_table)
+    ast_data = sc.storage_connection(str('/Users/shawnnettleton/Documents/credentials_shawn.json'))  # call storage_connection() to get the ast
+    root, lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
+    in_order_list = tr.in_order_traversal(root)  # call in_order_traversal() to get the sequence.
+    subSequence_list = w2v.sequenceSplit(in_order_list, lookup_table)
 
-    for list in range(len(subSequence_list)):
-        print(subSequence_list[list])
+    tokenized_subSequence, tokenized_lookup_table = w2v.tokenization(subSequence_list)
+    print(tokenized_lookup_table)
+    print(tokenized_subSequence)
+    # for list in range(len(subSequence_list)):
+    #     print(subSequence_list[list])
