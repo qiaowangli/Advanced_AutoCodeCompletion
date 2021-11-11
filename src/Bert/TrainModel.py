19512bcfd56ea459625c642cf462f2cd98289259
Samsara <alex116.dll@gmail.com>
2021-11-11 14:56:43 -0800

    added model trainig

--- src/Bert/TrainModel.py
+++ src/Bert/TrainModel.py
@@ -1,92 +1,118 @@
-from transformers import BertConfig, BertLMHeadModel
+from transformers import BertConfig, BertForPreTraining
 from transformers import BertTokenizer
-from torch.utils.data import Dataset, DataLoader
-from transformers import Trainer, TrainingArguments
-from transformers import RobertaTokenizer, RobertaForMaskedLM, pipeline
+from transformers import AdamW
+from tqdm import tqdm
 
-import torch
 import pickle
-import numpy as np
-import json
-import os
+import random
+import torch
 
-#Reference: https://www.kaggle.com/mojammel/train-model-from-scratch-with-huggingface
+MAX_LEN = 512
 
-class CodeDataset(Dataset):
-    def __init__(self,input,mask):
-        self.inputs = np.array(input)
-        self.mask = np.array(mask)
+class CodeDataSet(torch.utils.data.Dataset):
+    def __init__(self,encodings):
+        self.encodings = encodings
+
+    def __getitem__(self,idx):
+        return {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
 
     def __len__(self):
-        return len(self.inputs)
-
-    def __getitem__(self, idx):
-        src = self.inputs[idx]
-        mask = self.mask[idx]
-        return src, mask
+        return len(self.encodings.input_ids)
 
 def makeModel(tokenizer):
     #TODO: better bert config
     config = BertConfig(tokenizer.vocab_size, hidden_size=300,
                         num_hidden_layers=2, num_attention_heads=2, is_decoder=True,
                         add_cross_attention=True)
-    model = BertLMHeadModel(config)
+    model = BertForPreTraining (config)
     return model
 
-def eval_model(model,tokenizer):
-    code_example = "printStatus CallExpression [MASK] BlockStatement FunctionDeclaration Program"
-    fill_mask = pipeline('fill-mask', model=model, tokenizer=tokenizer)
-    outputs = fill_mask(code_example)
-    for i in outputs:
-        print(i)
+def MakeNSPinput(data):
+    '''
+    Generate train data for NSP training
+    input: data is the list sequences (while seq is a list of words)
+    output: first and second setence pair with label 0 for true and 1 for false
+    '''
+    firstHalf = []
+    secondHalf = []
+    #0 -> a and b does connect 1 -> they don't
+    labels = []
+    for i in range(len(data)):
+        if(random.random() > 0.4):
+            #Correct split
+            labels.append(0)
+            splitPoint = random.randint(1,len(data[i])-1)
+            firstHalf.append(' '.join(data[i][:splitPoint]))
+            secondHalf.append(' '.join(data[i][splitPoint:]))
+        else:
+            #random split
+            labels.append(1)
+            otherList = random.randint(0,len(data)-1)
+            while otherList == i:
+                otherList = random.randint(0,len(data)-1)
+            splitPoint = random.randint(1,len(data[i])-1)
+            firstHalf.append(' '.join(data[i][:splitPoint]))
+            splitPoint = random.randint(1,len(data[otherList])-1)
+            secondHalf.append(' '.join(data[otherList][splitPoint:]))
+    labels = torch.LongTensor([labels]).t
+    return firstHalf,secondHalf,labels
 
-def data_collate_fn(features):
-    batch = {}
-    batch['input_ids'] = torch.tensor([t[0] for t in features])
-    batch['attention_mask'] = torch.tensor([t[1] for t in features])
-    return batch
+def MakeMLMMasking(inputs,maskingRate = 0.15):
+    rand = torch.rand(inputs.input_ids.shape)
+    #Do not mask CLS, SEP, or PAD
+    mask_arr = (rand < 0.15) * (inputs.input_ids != 3) * (inputs.input_ids != 4) * (inputs.input_ids != 0)
+    for i in range(inputs.input_ids.shape[0]):
+        selection = torch.flatten(mask_arr[i].nonzero()).tolist()
+        #MASK id
+        inputs.input_ids[i,selection] = 5
+    return inputs
 
 def main():
     MAX_LEN = 15
+    #data is the list sequences (while seq is a list of words)
     data = pickle.load(open("data.p","rb"))
-    sentences = []
-    with open("Sentences.txt","r") as f:
-        sentences = f.read().split("\n")
+    firstHalf,secondHalf,labels = MakeNSPinput(data)
 
     tokenizer = BertTokenizer.from_pretrained('./CodeTokenizer')
-    #tokenizedData = [tokenizer(l,pad_to_max_length=True,max_length=MAX_LEN,add_special_tokens =True) for l in sentences]
-    tokenizedData = tokenizer(sentences,padding="longest",max_length=MAX_LEN)
-    input = [d for d in tokenizedData["input_ids"]]
-    mask = [d for d in tokenizedData["attention_mask"]]
-    dataset = CodeDataset(input, mask)
+    inputs = tokenizer(firstHalf,secondHalf,return_tensors="pt",
+                        max_length=MAX_LEN,truncation=True,padding="max_length")
+    inputs["next_sentence_label"] = labels
+    inputs["labels"] = inputs.input_ids.detach().clone()
 
-    #data_collate_fn(tokenizer,data)
+    inputs = MakeMLMMasking(inputs)
+    dataset = CodeDataSet(inputs)
+    print (dataset[3])
+    exit(0)
+
+    loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True)
+    #device = torch.device("cuba") if torch.cuba.is_available() else torch.device("cpu")
+    device = torch.device("cpu")
+
     model = makeModel(tokenizer)
+    model.to(device)
+    model.train()
+    optimizer = AdamW(model.parameters(),lr=5e-5)
 
+    for epoch in range(2):
+        loop = tqdm(loader,leave=True)
+        for batch in loop:
+            optim.zero_grad()
+            input_ids = batch["input_ids"].to(device)
+            token_type_ids = batch["token_type_ids"].to(device)
+            attention_mask = batch["attention_mask"].to(device)
+            next_sentence_label = batch["next_sentence_label"].to(device)
+            labels = batch["labels"].to(device)
 
+            outputs = model(input_ids,token_type_ids=token_type_ids,
+                            attention_mask=attention_mask,
+                            next_sentence_label=next_sentence_label,
+                            labels=labels)
+            loss = outputs.loss
+            loss.backward()
+            optim.step()
 
-
-    training_args = TrainingArguments(
-        output_dir="./bert",
-        overwrite_output_dir=True,
-        num_train_epochs=10,
-        do_train=True,
-        per_gpu_train_batch_size=32,
-        save_steps=500,
-        save_total_limit=2
-    )
-
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=dataset,
-        eval_dataset=dataset,
-        data_collator=data_collate_fn
-    )
-
-    trainer.train()
-
-
+            loop.set_description(f'Epoch {epoch}')
+            loop.set_postfix(loss=loss.item)
 
 if __name__ == '__main__':
     main()
