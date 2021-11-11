0000c2cdc33f2a42472cc233e19806104eb65e1d
Roy <liqiaowangg@gmail.com>
2021-11-10 23:15:51 -0800

    skipgram implementation first version

--- src/test.py
+++ src/test.py
@@ -2,21 +2,38 @@
 import storage_connection as sc
 import Word2Vec as w2v
 import traversal as tr
+import word_embedding as we
 import sys
 
 """
-Last edited by   : Shawn
-Last edited time : 09/11/2021
+Last edited by   : Roy
+Last edited time : 10/11/2021
 Version Status: dev
 TO DO: Verify correctness
 """
 
 if __name__ == "__main__":
     ast_data = sc.storage_connection(str(sys.argv[1]))  # call storage_connection() to get the ast
-    root, lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
-    in_order_list = tr.in_order_traversal(root)  # call in_order_traversal() to get the sequence.
-    subSequence_list = w2v.sequenceSplit(in_order_list, lookup_table)
+    # root, lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
+    # in_order_list = tr.in_order_traversal(root)  # call in_order_traversal() to get the sequence.
+    # subSequence_list = w2v.sequenceSplit(in_order_list, lookup_table)
 
-    tokenized_subSequence, tokenized_lookup_table = w2v.tokenization(subSequence_list)
-    print(len(tokenized_lookup_table))
-    # print(tokenized_subSequence)
+    # # tokenized_subSequence, tokenized_lookup_table = w2v.tokenization(subSequence_list)
+    # # print(tokenized_subSequence)
+    # # # embedding_table=we.word_embedding(tokenized_subSequence, tokenized_lookup_table)
+    # print(subSequence_list)
+
+    seq_table={} # key: id, value: sub_seq
+    seq_table_index=0
+    for ast_id in range(len(ast_data)):
+        root,lookup_table = w2v.ast2lcrs(ast_data[ast_id])  # call ast2lcrs() to convert the ast to lcrs
+        in_order_list = tr.in_order_traversal(root) # call in_order_traversal() to get the sequence.
+        subSequence_list=w2v.sequenceSplit(in_order_list,lookup_table)
+        for list in range(len(subSequence_list)):
+            seq_table[seq_table_index]=subSequence_list[list]
+            seq_table_index+=1
+
+    tokenized_subSequence, tokenized_lookup_table = w2v.tokenization(seq_table)
+    embedding_table=we.word_embedding(tokenized_subSequence, tokenized_lookup_table)
+    print(subSequence_list)
+    
--- src/word_embedding.py
+++ src/word_embedding.py
@@ -1,25 +1,119 @@
 #!/usr/bin/python3
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 """
 Last edited by   : Roy
-Last edited time : 09/11/2021
+Last edited time : 10/11/2021
 Version Status: dev
 TO DO: Skipgram implementaion with tf
 """
 
+class Word2Vec(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim,neg_example):
+    super(Word2Vec, self).__init__()
+    self.target_embedding = tf.keras.layers.Embedding(vocab_size,
+                                      embedding_dim,
+                                      input_length=1,
+                                      name="w2v_embedding")
+    self.context_embedding = tf.keras.layers.Embedding(vocab_size,
+                                       embedding_dim,
+                                       input_length=neg_example+1)
 
-def storage_connection(tokenized_subsequence, lookup_table_dict):
+  def call(self, pair):
+    target, context = pair
+    # target: (batch, dummy?)  # The dummy axis doesn't exist in TF2.7+
+    # context: (batch, context)
+    if len(target.shape) == 2:
+      target = tf.squeeze(target, axis=1)
+    # target: (batch,)
+    word_emb = self.target_embedding(target)
+    # word_emb: (batch, embed)
+    context_emb = self.context_embedding(context)
+    # context_emb: (batch, context, embed)
+    dots = tf.einsum('be,bce->bc', word_emb, context_emb)
+    # dots: (batch, context)
+    return dots
+
+#reference: https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/sequence/skipgrams
+
+def generate_training_data(sequences, window_size, num_ns, vocab_size):
+  # Elements of each training example are appended to these lists.
+  targets, contexts, labels = [], [], []
+
+  # # Build the sampling table for vocab_size tokens.
+  # sampling_table = tf.keras.preprocessing.sequence.make_sampling_table(vocab_size)
+
+  # Iterate over all sequences (sentences) in dataset.
+  for sequence in sequences:
+
+    # Generate positive skip-gram pairs for a sequence (sentence).
+    positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
+          sequence,
+          vocabulary_size=vocab_size,
+          window_size=window_size,
+          negative_samples=0)
+
+    # Iterate over each positive skip-gram pair to produce training examples
+    # with positive context word and negative samples.
+    for target_word, context_word in positive_skip_grams:
+      context_class = tf.expand_dims(
+          tf.constant([context_word], dtype="int64"), 1)
+      negative_sampling_candidates, _, _ = tf.random.log_uniform_candidate_sampler(
+          true_classes=context_class,
+          num_true=1,
+          num_sampled=num_ns,
+          unique=True,
+          range_max=vocab_size,
+          seed=10,
+          name="negative_sampling")
+
+      # Build context and label vectors (for one target word)
+      negative_sampling_candidates = tf.expand_dims(
+          negative_sampling_candidates, 1)
+
+      context = tf.concat([context_class, negative_sampling_candidates], 0)
+      label = tf.constant([1] + [0]*num_ns, dtype="int64")
+
+      # Append each element from the training example to global lists.
+      targets.append(target_word)
+      contexts.append(context)
+      labels.append(label)
+
+  return targets, contexts, labels
+
+def word_embedding(tokenized_subsequence, lookup_table_dict):
     """
     @ input : tokenized_dict --> input dict just replace words with int representation
                 and lookup_table_dict --> {1, [R = 'Program, F = 12],...}
     @ output: word embedding table
     """
-    window_size = 2
-    positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
-        tokenized_subsequence[0],
-        vocabulary_size=len(lookup_table_dict),
-        window_size=window_size,
-        negative_samples=0)
-    print(len(positive_skip_grams))
+    targets, contexts, labels=generate_training_data(tokenized_subsequence,2,2,len(tokenized_subsequence))
+    AUTOTUNE = tf.data.AUTOTUNE
+    targets = np.array(targets)
+    contexts = np.array(contexts)[:,:,0]
+    labels = np.array(labels)
+    BATCH_SIZE = 1024
+    BUFFER_SIZE = 10000
+    dataset = tf.data.Dataset.from_tensor_slices(((targets, contexts), labels))
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+    # acclerating the processing speed 
+    dataset = dataset.cache().prefetch(buffer_size=AUTOTUNE)
+    # constructing embedding table 
+    embedding_dim = 300
+    # the size of embedding table would be len(lookup_table_dict) * embedding_dim
+    word2vec = Word2Vec(len(lookup_table_dict), embedding_dim,10)
+    word2vec.compile(optimizer='adam',
+                    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                 metrics=['accuracy'])
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="logs")
+    # fitting model
+    word2vec.fit(dataset, epochs=20, callbacks=[tensorboard_callback])
+    # output embedding table
+    return word2vec.get_layer('w2v_embedding').get_weights()[0]
+
+
+
+
+    
 
