d2b14252ce9d068f87b6178003b381eb9c3507cb
Roy <royli@w134-87-049-248.wireless.uvic.ca>
2021-11-13 12:19:59 -0800

    create init for bert and import to test.py

--- src/test.py
+++ src/test.py
@@ -4,20 +4,19 @@ import Word2Vec as w2v
 import traversal as tr
 import sys
 import pickle
+from Bert.Bert import trainBertModel
 
 """
 Last edited by   : Roy
-Last edited time : 08/11/2021
+Last edited time : 13/11/2021
 Version Status: stable
 TO DO: Verify correctness
 """
 
 if __name__ == "__main__":
-    ast_data = sc.storage_connection(str(sys.argv[1]),"top100.json")  # call storage_connection() to get the ast
-    root,lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
-    in_order_list = tr.in_order_traversal(root) # call in_order_traversal() to get the sequence.
-    subSequence_list=w2v.sequenceSplit(in_order_list,lookup_table)
+    # ast_data = sc.storage_connection(str(sys.argv[1]),"top100.json")  # call storage_connection() to get the ast
+    # root,lookup_table = w2v.ast2lcrs(ast_data[0])  # call ast2lcrs() to convert the ast to lcrs
+    # in_order_list = tr.in_order_traversal(root) # call in_order_traversal() to get the sequence.
+    # subSequence_list=w2v.sequenceSplit(in_order_list,lookup_table)
+    pass
 
-    #pickle.dump(subSequence_list,open("inputList.p","wb"))
-    for list in range(len(subSequence_list)):
-       print(subSequence_list[list])
