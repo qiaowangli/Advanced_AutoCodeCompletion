ac30097a1049abd96bf5a7dca523f462723d9d47
Roy <liqiaowangg@gmail.com>
2021-11-14 14:17:29 -0800

    use the pd library

--- src/RNN/RNN_input.py
+++ src/RNN/RNN_input.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# from sklearn.decomposition import PCA
 from sklearn.decomposition import PCA
 import numpy as np
-from csv import reader
+import pandas as pd
+
 
 
 """
@@ -11,21 +11,12 @@ Last edited time : 13/11/2021
 Version Status: dev
 TO DO: NONE
 """
-def csv_convert(file_path,row_zie,col_size,pca_variance=0.9):
+def csv_convert(file_path,pca_variance=0.9):
     """
-    @ input : file_path,row_zie,col_size,pca_variance
+    @ input : file_path,pca_variance
     @ output: reducted numpy dataframe
     """
-    dataframe=np.zeros((row_zie,col_size))
-    with open('/Users/royli/Desktop/vector.csv', 'r') as read_obj:
-        # Create a csv.reader object from the input file object
-        csv_reader = reader(read_obj)
-        row_tracker=0
-        for index in csv_reader:
-            vec_list=index[0].split()
-            for value_index in range(len(vec_list)):
-                dataframe[row_tracker][value_index]=float(vec_list[value_index])
-            row_tracker+=1
+    dataframe=pd.read_csv(file_path)
     
     # reduce the dimentions given the pca_variance value and return the reducted dataframe
     pca = PCA(n_components=pca_variance)
