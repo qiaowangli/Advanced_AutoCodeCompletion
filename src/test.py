2f852f9dfc6102b4f6745c745cf3d563fe358216
Roy <liqiaowangg@gmail.com>
2021-11-25 11:52:59 -0800

    typo fixed

--- src/test.py
+++ src/test.py
@@ -13,7 +13,6 @@ TO DO: add the command for bert
 """
 
 if __name__ == "__main__":
-    # ast_data = sc.storage_connection('/Users/shawnnettleton/Documents/credentials_shawn.json', "top100.json")  # call storage_connection() to get the ast
     ast_data = sc.storage_connection(str(sys.argv[1]), "top100.json")  # call storage_connection() to get the ast
 
     seq_table = {}  # key: id, value: sub_seq
@@ -31,6 +30,7 @@ if __name__ == "__main__":
     To overcome this timing issue, we outputed the embedding_table to vector.csv that could be found in S3 storage, for further construction, checkout the RNN folder
     """
     tokenized_subSequence, tokenized_lookup_table = seq_produce.tokenization(seq_table)
+    # embedding_table = we.word_embedding(tokenized_subSequence, tokenized_lookup_table)
 
     # This takes care of step 1 that we discussed
     # file = ''  # Need to add the filepath for the requested CSV file
@@ -45,9 +45,3 @@ if __name__ == "__main__":
         output_file.write(str(sequence))
         output_file.write('\n')
     output_file.close()
-
-    # embedding_table = we.word_embedding(tokenized_subSequence, tokenized_lookup_table)
-
-    """
-    The seq_table would be the input for bert.
-    """
