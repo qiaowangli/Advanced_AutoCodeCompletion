df01cf2a9ee6d8b9061f0ce6da6aeb5baf247dbb
Samsara <alex116.dll@gmail.com>
2021-12-08 15:36:54 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -6,10 +6,10 @@ import traversal as tr
 import sys
 
 """
-Last edited by   : Shawn
-Last edited time : 15/11/2021
+Last edited by   : Roy
+Last edited time : 08/12/2021
 Version Status: dev
-TO DO: add the command for bert
+TO DO:
 """
 
 if __name__ == "__main__":
@@ -22,11 +22,11 @@ if __name__ == "__main__":
             root, lookup_table = seq_produce.ast2lcrs(ast_data[ast_id])  # call ast2lcrs() to convert the ast to lcrs
             in_order_list = tr.in_order_traversal(root)  # call in_order_traversal() to get the sequence.
             subSequence_list = seq_produce.sequenceSplit(in_order_list, lookup_table)
-            for list in range(len(subSequence_list)):
-                seq_table[seq_table_index] = subSequence_list[list]
-                seq_table_index += 1
-        except:
+        except RecursionError as err:
             pass
+        for list in range(len(subSequence_list)):
+            seq_table[seq_table_index] = subSequence_list[list]
+            seq_table_index += 1
 
     """
     the following command would take 3-6 hours to produce the embedding_table for RNN/LSTM given the tokenized_subSequence and tokenized_lookup_table,
@@ -43,7 +43,6 @@ if __name__ == "__main__":
             print(i)
     pickle.dump(data,open("top100.p","wb"))
     exit(0)
-
 
     tokenized_subSequence, tokenized_lookup_table = seq_produce.tokenization(seq_table)
     # embedding_table = we.word_embedding(tokenized_subSequence, tokenized_lookup_table)
